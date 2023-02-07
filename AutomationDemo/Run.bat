973f4de30fafd89874d1d1683d01005187b00bc7
flogginggreyhome <36091342+flogginggreyhome@users.noreply.github.com>
2023-02-07 17:23:09 -0500

    Update Run.bat

--- AutomationDemo/Run.bat
+++ AutomationDemo/Run.bat
@@ -1,2 +1,2 @@
 
-C:\Users\huntg\.nuget\packages\nunit.consolerunner\3.16.2\nunit.consolerunner.3.16.2\tools\nunit3-console C:\Users\huntg\source\repos\AutomationDemo\AutomationDemo\bin\Debug\net6.0\AutomationDemo.dll --where "cat == test"
+C:\Users\...\.nuget\packages\nunit.consolerunner\3.16.2\nunit.consolerunner.3.16.2\tools\nunit3-console C:\Users\...\source\repos\AutomationDemo\AutomationDemo\bin\Debug\net6.0\AutomationDemo.dll --where "cat == test"
